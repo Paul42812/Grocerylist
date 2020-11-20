05796c15e958471621a9372046950dc88fd47789
Paul42812 <paul.jaeger06@gmail.com>
2020-11-20 21:14:02 +0100

    added donelist

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Grocery List
-##### v. 0.4 by Paul42812
+##### v. 1 by Paul42812
 
 ### Features:
 * Add Items to list 
@@ -8,33 +8,6 @@
 * Import csv lists
 * Clear all items from the list
 * Configuration with the config.js
-
-<!--
-###Configuration
-* config.js
-	* labels:
-		* title: Title of the page
-		* settings_title: Title of configuration box
-		* list
-			* empty: Title of the empty listbox
-			* items : Title of listbox with items
-		* buttons
-			* add_item: add item button
-			* imp_list: import list button
-			* exp_list: export list button
-			* clear: clear button
-	* valid_file: 
-		* max_size: Max file size for import
-	* errors:
-		* file_upload:
-			* too_large: too large file is selected
-			* wrong_format: selected file has the wrong format
-			* no_sellection: No file is selected 
-		* add_itmes:
-			* empty: The textbox is empty
-			* alredy_there: The item is alredy in the list
--->
-
 
 ### Additional Informations:
 * Dont select to huge files (Your system could crash)
--- app.js
+++ app.js
@@ -1,56 +1,116 @@
 var mainlist;
+var selected;
+var selectedlist;
+var usedonelist = false;
 var items = [];
+var doneitems = [];
 
-timer = setInterval(updatesettings, 100);
+//timer = setInterval(updatescroller, 10);
+//clearInterval(timer);
 
 function update() {
     mainlist = document.getElementById("mainlist")
     mainlist.innerHTML = "";
 
+    donelist = document.getElementById("donelist")
+    donelist.innerHTML = "";
+
     for (let i = 0; i < items.length; i++) {
-        //mainlist.innerHTML += '<li id="' + i + '"><form><span id="' + i + 's' + '">' + items[i] + '</span><button type="button" id="' + i + 'b' + '"onClick="del(this.id)">X</button></form></li>'
-        mainlist.innerHTML += '<li><form><span id="' + i + 's' + '">' + items[i] + '</span><button type="button" id="' + i + '"onClick="del(this.id)" class="listbutton">X</button></form></li>'
+        mainlist.innerHTML +=
+            `<li>
+            <form id="` + i + `">
+                <span>` + items[i] + `</span>
+                <button type="button" onClick="del(this.parentElement.id, 0)" class="listbutton">X</button>
+                <button type="button" onClick="rename(this.parentElement.id, 0)" class="changebutton">Y</button>
+            </form>
+        </li>`
+    }
+
+    for (let i = 0; i < doneitems.length; i++) {
+        donelist.innerHTML +=
+            `<li>
+            <form id="` + i + `">
+                <span id="` + i + 'o' + '">' + doneitems[i] + `</span>
+                <button type="button" onClick="del(this.parentElement.id, 1)" class="listbutton">X</button>
+                <button type="button" onClick="rename(this.parentElement.id, 1)" class="changebutton">Y</button>
+            </form>
+        </li>`
     }
     save()
+
+    var r = document.querySelector(':root');
+    if (items.length == 0) {
+        r.style.setProperty('--show-main', "none");
+    } else {
+        r.style.setProperty('--show-main', "block");
+    }
+
+    if (doneitems.length == 0) {
+        r.style.setProperty('--show-done', "none")
+    } else {
+        r.style.setProperty('--show-done', "block")
+    }
 }
 
-function del(id) {
-    var name = document.getElementById(id + "s");
-    items.splice(items.indexOf(name.innerHTML), 1);
-    update();
+function del(id, listid) {
+    if (usedonelist) {
+        breakch();
+        if (listid == 0) {
+            doneitems.push(items[id]);
+            items.splice(id, 1);
+        }
+        if (listid == 1) {
+            doneitems.splice(id, 1);
+        }
+        update();
+    } else {
+        breakch();
+        items.splice(id, 1);
+        update();
+    }
 }
 
 function save() {
-    localStorage.setItem("save", items)
+    localStorage.setItem("items", items);
+    localStorage.setItem("doneitems", doneitems);
 }
 
 function load() {
     if (!(localStorage.length == 0)) {
-        var u = localStorage.getItem("save");
+        var u = localStorage.getItem("doneitems");
+        var spli = u.split(",");
+        if (!(spli[0] == "")) {
+            doneitems = spli;
+        }
+
+        var u = localStorage.getItem("items");
         var spli = u.split(",");
         if (!(spli[0] == "")) {
             items = spli;
-            update();
         }
+        update();
     }
 }
 
 function add() { //Adds an Item to the List
+    breakch();
     f = document.getElementById("additem").value;
     if (f.length == 0) {
         alert(json["errors"]["add_items"]["empty"]);
         document.getElementById("additem").value = "";
     } else if (!(items.indexOf(f) == -1)) {
-        alert(json["errors"]["add_items"]["alredy_there"])
+        alert(json["errors"]["add_items"]["alredy_there"]);
         document.getElementById("additem").value = "";
     } else {
         items.push(f);
         update()
         document.getElementById("additem").value = "";
+        document.getElementById("additem").focus();
     }
 }
 
 function exp() { //Downloads the Current List as csv File
+    breakch();
     if (items.length == 0) {
         alert(json["errors"]["exp"]["empty"])
     } else {
@@ -66,6 +126,7 @@ function exp() { //Downloads the Current List as csv File
 }
 
 function imp() {
+    breakch();
     var mf = document.getElementById("myfile")
     if (mf.value == mf.defaultValue) {
         alert(json["errors"]["file_upload"]["no_sellection"]);
@@ -100,8 +161,10 @@ function imp() {
 }
 
 function clr() {
-    items = []
-    update()
+    breakch();
+    items = [];
+    doneitems = [];
+    update();
 }
 
 function updatesettings() {
@@ -129,11 +192,79 @@ function updatesettings() {
 
     var clear = document.getElementById("clear");
     clear.innerHTML = json["labels"]["buttons"]["clear"];
+}
+
+function updatescroller() {
+    var r = document.querySelector(':root');
+    var scroll = Math.round(window.scrollY);
+    var srr = (scroll + 10).toString() + "px";
+    r.style.setProperty('--top-c', srr);
+}
+
+function rename(id, listid) {
+    breakch();
+    selected = id;
+    selectedlist = listid;
+    updatescroller()
+    timer = setInterval(updatescroller, 10);
+    var main = document.getElementById("main");
+
+    main.innerHTML +=
+        `<div id='ch'>
+        <form>
+            <input type='text' id='chname' onkeypress='return changename(event)'>
+            <button type='button' onclick='breakch()' class='button' id='brk'>X</button>
+        </form>
+    </div>`
+
+    if (listid == 0) {
+        document.getElementById("chname").value = items[id];
+    } else {
+        document.getElementById("chname").value = doneitems[id];
+    }
+    document.getElementById("chname").focus();
 
 }
 
-if (items.length == 0) {
-    load()
+function changename(e) {
+    e = e || window.event;
+    var key = e.keyCode;
+    if (key == 13) { //Enter
+        var value = document.getElementById("chname").value;
+        if (items.includes(value)) {
+            alert(json["errors"]["add_items"]["alredy_there"])
+        } else {
+            if (selectedlist == 0) {
+                items[selected] = value;
+            } else {
+                doneitems[selected] = value;
+            }
+            document.getElementById("ch").remove();
+            update();
+        }
+        return false;
+    }
 }
 
-update()
+function eadd(e) {
+    e = e || window.event;
+    var key = e.keyCode;
+    if (key == 13) { //Enter
+        add();
+        return false;
+    }
+}
+
+function breakch() {
+    try {
+        clearInterval(timer);
+    } catch (e) {}
+    var g = document.getElementById("ch");
+    try {
+        g.remove();
+    } catch (e) {}
+}
+
+load();
+update();
+updatesettings();
